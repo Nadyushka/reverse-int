ddd71d9fc52c8a29c4af084e9bd4345e8d8fbef7
Nadyushka <105865727+Nadyushka@users.noreply.github.com>
2022-06-25 12:15:07 +0300

    add: do task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(a) {
+	let b = Math.abs(a);
+	let arr = String(b).split('');
+	let result = [];
+	for (let i = 0; i < arr.length; i++) {
+		result.unshift(arr[i]);
+	}
+	return Number(result.join(''));
 }
